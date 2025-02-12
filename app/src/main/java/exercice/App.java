e8596dc786a772e99cabfc8c83c34ca3c7793ba5
Nicolas Theau <theaun91@gmail.com>
2025-02-12 14:48:13 +0100

    Ajout de la méthode getNDames et test d'un cas qui échoue

--- app/src/main/java/exercice/App.java
+++ app/src/main/java/exercice/App.java
@@ -1,5 +1,15 @@
 package exercice;
 
+import java.util.List;
+
 public class App {
+
+    public static void main(String[] args) {
+        Plateau plateau = new Plateau(1);
+    }
+
+    public static List<Plateau> getNDames(int n) {
+        return null;
+    }
 
 }
--- app/src/test/java/exercice/AppTest.java
+++ app/src/test/java/exercice/AppTest.java
@@ -1,11 +1,17 @@
 package exercice;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class AppTest {
 
     @Test
-    public void test1() {
-
+    public void getNDamesTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            App.getNDames(0);
+        });
     }
 }
